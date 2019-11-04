af43339cf375c91b004242cbcf17cfb772afbb87
dy9527 <s865529248@163.com>
2019-11-04 09:14:31 +0800

    test

--- src/main/java/cn/wzvtc/soft/TestController.java
+++ src/main/java/cn/wzvtc/soft/TestController.java
@@ -1,5 +1,6 @@
 package cn.wzvtc.soft;
 
+import org.apache.coyote.Response;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,20 +8,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
-
-@CrossOrigin
+//@CrossOrigin("*")
+@CrossOrigin(origins = {"http://10.30.201.71:5500","http://10.30.201.75:5500",
+        "http://10.30.201.74:5500","http://10.30.201.31:5500","http://127.0.0.1:5500"})
 @RestController("/" )
 public class TestController {
 
-    @RequestMapping(value="data0.json",method={RequestMethod.GET})
+    @RequestMapping(value="data.json",method={RequestMethod.GET})
     public Map<String,String> bookById0(){
         Map<String,String> resultMap=new HashMap<>();
         resultMap.put("myname","苏威");
         resultMap.put("mynumber","18002090225");
         return resultMap;
-        
+
     }
-    @RequestMapping(value="data1.json",method={RequestMethod.GET})
+    @RequestMapping(value="data.json",method={RequestMethod.POST})
     public Map<String,String> bookById1(){
         Map<String,String> resultMap=new HashMap<>();
         resultMap.put("myname","XX");

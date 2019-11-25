3997b3a316e4aa932e5180d88ca1513a983b3ed4
DY9527 <s865529248@163.com>
2019-11-25 09:30:15 +0800

    test

--- src/main/java/cn/wzvtc/soft/TestController.java
+++ src/main/java/cn/wzvtc/soft/TestController.java
@@ -1,32 +1,39 @@
 package cn.wzvtc.soft;
 
 import org.apache.coyote.Response;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.Map;
-//@CrossOrigin("*")
+//@CrossOrigin
 @CrossOrigin(origins = {"http://10.30.201.71:5500","http://10.30.201.75:5500",
         "http://10.30.201.74:5500","http://10.30.201.31:5500","http://127.0.0.1:5500"})
 @RestController("/" )
 public class TestController {
 
-    @RequestMapping(value="data.json",method={RequestMethod.GET})
-    public Map<String,String> bookById0(){
+    @RequestMapping(value="/userinfo")
+    public Map<String,String> userinfo(HttpSession httpSession){
         Map<String,String> resultMap=new HashMap<>();
-        resultMap.put("myname","苏威");
-        resultMap.put("mynumber","18002090225");
+        if(httpSession.getAttribute("loginname")!=null){
+                resultMap.put("myname","苏威");
+                resultMap.put("mynumber","18002090225");
+        }
         return resultMap;
 
     }
-    @RequestMapping(value="data.json",method={RequestMethod.POST})
-    public Map<String,String> bookById1(){
+
+
+    @RequestMapping(value="/login")
+    public Map<String,String> login(@RequestBody Map<String,String> map, HttpSession httpSession){
+        String pwd=map.get("pwd");
+        String name=map.get("name");
+
         Map<String,String> resultMap=new HashMap<>();
-        resultMap.put("myname","XX");
-        resultMap.put("mynumber","180020902XX");
+        if("18002090225".equals(name) && "18002090225".equals(pwd)) {
+            httpSession.setAttribute("loginname",name);
+            resultMap.put("result","success");
+        }
         return resultMap;
     }
 }
